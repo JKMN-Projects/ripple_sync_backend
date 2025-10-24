d9cbfc58db70b3055c2ac6826d5b61d6c645c44f
Kevin Borup - Mobile <kevin.borup@outlook.com>
2025-10-24 17:23:48 +0200

    DbMigrator implementeret

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -2,8 +2,27 @@ using RippleSync.API.Common.Middleware;
 using RippleSync.Application;
 using RippleSync.Infrastructure;
 using Serilog;
+using DbMigrator;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+/// Need connectionString
+//string? connString = builder.Configuration.GetConnectionString("Postgres");
+
+//if (string.IsNullOrWhiteSpace(connString))
+//{
+//    throw new InvalidOperationException("Connection string not found");
+//}
+
+//if (builder.Environment.IsDevelopment())
+//    if (DatabaseMigrator.MigrateDatabase(connString, true) == 0)
+//        DatabaseMigrator.MigrateDatabase(connString);
+
 builder.Host.UseDefaultServiceProvider(options =>
 {
     if (builder.Environment.IsDevelopment())
@@ -13,10 +32,6 @@ builder.Host.UseDefaultServiceProvider(options =>
     }
 });
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -25,10 +40,10 @@ builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
 builder.Services.AddExceptionHandler<GlobalExceptionHandling>();
 
-builder.Services.AddSerilog(options =>
-{
-    options.ReadFrom.Configuration(builder.Configuration);
-});
+//builder.Services.AddSerilog(options =>
+//{
+//    options.ReadFrom.Configuration(builder.Configuration);
+//});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructure();
--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RippleSync.Domain.Users;
+﻿namespace RippleSync.Domain.Users;
 public class User(Guid id, string email)
 {
     public Guid Id { get; set; } = id;
