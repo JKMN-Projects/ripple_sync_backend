c25412bacf3e6cd111f2e24d2b11136a6dedd6be
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-10-24 14:39:02 +0200

    Added serilog

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,13 +1,34 @@
+using RippleSync.API.Common.Middleware;
 using RippleSync.Application;
 using RippleSync.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseDefaultServiceProvider(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        options.ValidateScopes = true;
+        options.ValidateOnBuild = true;
+    }
+});
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandling>();
+
+builder.Services.AddSerilog(options =>
+{
+    options.ReadFrom.Configuration(builder.Configuration);
+});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructure();
