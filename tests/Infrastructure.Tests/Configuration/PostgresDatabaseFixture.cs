a4a88dfecbfdbfef7ccf01516a2dd8b57e86cb9f
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-11-05 12:39:08 +0100

    Added testbase and database fixture

---
+++ tests/Infrastructure.Tests/Configuration/PostgresDatabaseFixture.cs
@@ -0,0 +1,29 @@
+﻿
+using DbMigrator;
+using Testcontainers.PostgreSql;
+
+namespace RippleSync.Infrastructure.Tests.Configuration;
+
+internal class PostgresDatabaseFixture : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer postgreSqlContainer = new PostgreSqlBuilder()
+        .WithDatabase("ripple_sync_test_db")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    public string ConnectionString => postgreSqlContainer.GetConnectionString() + ";Include Error Detail=True";
+
+    public async Task InitializeAsync()
+    {
+        await postgreSqlContainer.StartAsync();
+
+        DatabaseMigrator.MigrateDatabase(ConnectionString);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await postgreSqlContainer.StopAsync();
+        await postgreSqlContainer.DisposeAsync();
+    }
+}
---
+++ tests/Infrastructure.Tests/Configuration/RepositoryTestBase.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using Respawn;
+using System.Data;
+
+namespace RippleSync.Infrastructure.Tests.Configuration;
+
+internal abstract class RepositoryTestBase : IAsyncLifetime, IClassFixture<PostgresDatabaseFixture>
+{
+    private readonly PostgresDatabaseFixture _fixture;
+    private Respawner _respawner = default!;
+    protected NpgsqlConnection DbConnection { get; private set; } = default!;
+    protected RepositoryTestBase(PostgresDatabaseFixture fixture)
+    {
+        _fixture = fixture;
+        DbConnection = new NpgsqlConnection(_fixture.ConnectionString);
+    }
+
+    protected async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_fixture.ConnectionString);
+    }
+
+    public async Task InitializeAsync()
+    {
+        if (DbConnection.State != ConnectionState.Open)
+        {
+            DbConnection.Open();
+        }
+
+        _respawner = await Respawner.CreateAsync(DbConnection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = ["public"],
+            TablesToIgnore = [
+                //new Table()
+            ]
+        });
+    }
+    public Task DisposeAsync() => Task.CompletedTask;
+}
