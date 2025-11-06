5507e82e69fcefdc3e3df01cfa016fca87c57f73
Hova <67004035+HovaUseless@users.noreply.github.com>
2025-11-06 17:59:12 +0100

    Setup test base

---
+++ tests/Application.Tests/PostManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using RippleSync.Application.Common.Queries;
+using RippleSync.Application.Common.Repositories;
+using RippleSync.Application.Platforms;
+using RippleSync.Application.Posts;
+using RippleSync.Tests.Shared.TestDoubles.Logging;
+using RippleSync.Tests.Shared.TestDoubles.Repositories;
+
+namespace RippleSync.Application.Tests;
+
+public abstract class PostManagerTests
+{
+    protected PostManager GetSystemUnderTest(
+        ILogger<PostManager>? logger = null,
+        IPostRepository? postRepository = null,
+        IPostQueries postQueries = null,
+        IIntegrationRepository? integrationRepository = null,
+        IPlatformFactory? platformFactory = null)
+    {
+        logger ??= new LoggerDoubles.Fakes.FakeLogger<PostManager>();
+        postRepository ??= new PostRepositoryDoubles.Dummy();
+        //postQueries ??= new PostQueriesDoubles.Dummy();
+        integrationRepository ??= new IntegrationRepositoryDoubles.Dummy();
+        //platformFactory ??= new PlatformFactoryDoubles.Dummy();
+
+        return new PostManager(
+            logger,
+            postRepository,
+            postQueries,
+            integrationRepository,
+            platformFactory
+        );
+    }
+
+    public sealed class ProcessPostEventAsync : PostManagerTests
+    {
+
+    }
+}
